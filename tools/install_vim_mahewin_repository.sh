11fdf04c23caafdf95e752b2e91f1e48d60cf02e
Hobbestigrou <hobbestigrou@erakis.im>
2012-01-15 22:07:47 +0100

    [Scripts] install_vim_mahewin_repository : Typo.

--- tools/install_vim_mahewin_repository.sh
+++ tools/install_vim_mahewin_repository.sh
@@ -7,7 +7,7 @@ function _check_user {
 
     if [ "$UID" -eq "$ROOT_UID" ]
     then
-        echo "You don't must be root to execute this script"
+        echo "You must not be root to execute this script"
         exit 0
     fi
 
@@ -29,39 +29,47 @@ function _install {
 
     if [ $install_type == 1 ];
     then
-        echo "Install vimrc from vim-mahewin-repository in your home"
-        cp $path_vim_mahewin_repository/vimrc $HOME/.vimrc
-
-        echo "Delete actually vim directory"
-        rm -rf $HOME/.vim
-        echo "Install vim directory from vim-mahewin-repository in your home"
-        cp -r $path_vim_mahewin_repository/vim $HOME/.vim
-
-        _build_command_t
+        _install_all
     else
-        files=$path_vim_mahewin_repository/vim/plugin/*
-        echo "Delete actually vim directory"
-        rm -rf $HOME/.vim
-        cp -r $path_vim_mahewin_repository/vim $HOME/.vim
-
-        rm -rf $HOME/.vim/plugin
-        mkdir $HOME/.vim/plugin
-
-        for file in $files
-        do
-            echo "Do you wan't install $file"
-            read install_plugin
-            if [ $install_plugin == 1 ];
-            then
-                echo "Install $file"
-                cp $file $HOME/.vim/plugin/
-                if [ "$file" == "$path_vim_mahewin_repository/vim/plugin/command-t.vim" ];
-                then
-                    _build_command_t
-                fi
-            fi
-        done
+        _interactive_install
     fi
+}
+
+function _interactive_install {
+    files=$path_vim_mahewin_repository/vim/plugin/*
+    echo "Delete actually vim directory"
+    rm -rf $HOME/.vim
+    cp -r $path_vim_mahewin_repository/vim $HOME/.vim
+
+    rm -rf $HOME/.vim/plugin
+    mkdir $HOME/.vim/plugin
+
+    for file in $files
+    do
+        echo "Do you wan't install $file"
+        read install_plugin
+        if [ $install_plugin == 1 ];
+        then
+            echo "Install $file"
+            cp $file $HOME/.vim/plugin/
+            if [ "$file" == "$path_vim_mahewin_repository/vim/plugin/command-t.vim" ];
+            then
+                _build_command_t
+            fi
+        fi
+    done
+}
+
+function _install_all {
+    echo "Install vimrc from vim-mahewin-repository in your home"
+    cp $path_vim_mahewin_repository/vimrc $HOME/.vimrc
+
+    echo "Delete actually vim directory"
+    rm -rf $HOME/.vim
+    echo "Install vim directory from vim-mahewin-repository in your home"
+    cp -r $path_vim_mahewin_repository/vim $HOME/.vim
+
+    _build_command_t
 }
 
 _check_user
