c6e316131c6bf867166cbe2fd2e63f6686a081af
Hobbestigrou <hobbestigrou@erakis.im>
2012-01-15 19:36:40 +0100

    [Scripts] Install_vim_mahewin_repository: Add interfactive install for plugin.

--- tools/install_vim_mahewin_repository.sh
+++ tools/install_vim_mahewin_repository.sh
@@ -24,16 +24,44 @@ function _build_command_t {
 
 function _install {
     echo "Thanks to use Vim-Mahewin-Repository"
+    echo "Do you want install all plugin or one by one"
+    read install_type
 
-    echo "Install vimrc from vim-mahewin-repository in your home"
-    cp $path_vim_mahewin_repository/vimrc $HOME/.vimrc
+    if [ $install_type == 1 ];
+    then
+        echo "Install vimrc from vim-mahewin-repository in your home"
+        cp $path_vim_mahewin_repository/vimrc $HOME/.vimrc
 
-    echo "Delete actually vim directory"
-    rm -rf $HOME/.vim
-    echo "Install vim directory from vim-mahewin-repository in your home"
-    cp -r $path_vim_mahewin_repository/vim $HOME/.vim
+        echo "Delete actually vim directory"
+        rm -rf $HOME/.vim
+        echo "Install vim directory from vim-mahewin-repository in your home"
+        cp -r $path_vim_mahewin_repository/vim $HOME/.vim
 
-    _build_command_t
+        _build_command_t
+    else
+        files=$path_vim_mahewin_repository/vim/plugin/*
+        echo "Delete actually vim directory"
+        rm -rf $HOME/.vim
+        cp -r $path_vim_mahewin_repository/vim $HOME/.vim
+
+        rm -rf $HOME/.vim/plugin
+        mkdir $HOME/.vim/plugin
+
+        for file in $files
+        do
+            echo "Do you wan't install $file"
+            read install_plugin
+            if [ $install_plugin == 1 ];
+            then
+                echo "Install $file"
+                cp $file $HOME/.vim/plugin/
+                if [ "$file" == "$path_vim_mahewin_repository/vim/plugin/command-t.vim" ];
+                then
+                    _build_command_t
+                fi
+            fi
+        done
+    fi
 }
 
 _check_user
