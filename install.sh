d016f5696acba3a343fe23099f69f2ec3b800df1
Hobbestigrou <hobbestigrou@erakis.eu>
2013-11-11 23:40:58 +0100

    [Scripts] Add a function to install via wget or curl (fixes #12).

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-function validate() {
-    if [ ! -e $HOME/.vim-mahewin-repository/ ]
-    then
-        echo 'You need have a .vim-mahewin-repository directory'
-        exit
-    fi
+#Clone vim-mahewin-repository to install
+function install_mahewin_repository() {
+    git clone https://github.com/hobbestigrou/Vim-Mahewin-Repository.git $HOME/.vim-mahewin-repository
+    ln -s $HOME/.vim-mahewin-repository/vimrc $HOME/.vimrc
 }
 
 
@@ -14,11 +12,10 @@ function install_vundle() {
     if [ ! -e "$HOME/.vim" ]
     then
         mkdir "$HOME/.vim"
-        git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+        git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
     fi
     vim +BundleInstall +qall
 }
 
-validate
-ln -s $HOME/.vim-mahewin-repository/vimrc $HOME/.vimrc
+install_mahewin_repository
 install_vundle
