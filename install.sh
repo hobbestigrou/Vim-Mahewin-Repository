53deffb58d594d29913b5012b7d5c2fe5b08fe35
hobbestigrou <natal.ngetal@novapost.fr>
2015-03-10 15:33:29 +0100

    [Scripts] Manage the error.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 function install_mahewin_repository() {
     if [[ "$(which ranger)" == "" ]]
     then
-        echo "Warning ranger is not installed, so not possible to us <leader>r"
+        echo "Warning ranger is not installed, so not possible to use <leader>r"
     fi
 
     check_vim
@@ -13,6 +13,11 @@ function install_mahewin_repository() {
     if [[ -e "$HOME/.vim-mahewin-repository" ]]
     then
         rm -rf "$HOME/.vim-mahewin-repository"
+        if [[ $? -ne 0 ]]
+        then
+            echo "Can not remove the .vim-mahewin-repository directory"
+            exit 1
+        fi
     fi
 
     git clone https://github.com/hobbestigrou/Vim-Mahewin-Repository.git $HOME/.vim-mahewin-repository
@@ -21,19 +26,56 @@ function install_mahewin_repository() {
 
 #Check if a vimrc file exist
 function manage_vimrc_file() {
+    if [[ -L "$HOME/.vimrc" ]]
+    then
+        rm $HOME/.vimrc
+        if [[ $? -ne 0 ]]
+        then
+            echo "Can not remove the symbolic .vimrc link"
+            exit 1
+        fi
+    fi
+
     if [[ -e "$HOME/.vimrc" ]]
     then
         read -p "You have vim configuration file, did you want use it with vim-mahewin-repository, say no if vim-mahewin-repository is already use(Y, y): "
         if [[ $REPLY =~ ^[Yy]$ ]]
         then
             mv $HOME/.vimrc $HOME/.vimrc.local
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not rename the .vimrc file to .vimrc.local"
+                exit 1
+            fi
+
             ln -s $HOME/.vim-mahewin-repository/vimrc $HOME/.vimrc
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not make the .vimrc symbolic link"
+                exit 1
+            fi
         else
             rm $HOME/.vimrc
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not remove the .vimrc file"
+                exit 1
+            fi
+
             ln -s $HOME/.vim-mahewin-repository/vimrc $HOME/.vimrc
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not make the .vimrc symbolic link"
+                exit 1
+            fi
         fi
     else
         ln -s $HOME/.vim-mahewin-repository/vimrc $HOME/.vimrc
+        if [[ $? -ne 0 ]]
+        then
+            echo "Can not make the .vimrc symbolic link"
+            exit 1
+        fi
     fi
 }
 
@@ -42,16 +84,37 @@ function install_vundle() {
     if [[ ! -e "$HOME/.vim" ]]
     then
         mkdir "$HOME/.vim"
+        if [[ $? -ne 0 ]]
+        then
+            echo "Can not create the .vim directory"
+            exit 1
+        fi
     else
         read -p "You have a .vim directory, do you want remove the directory or not(Y, y): "
         if [[ $REPLY =~ ^[Yy]$ ]]
         then
             rm -rf "$HOME/.vim"
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not remove the .vim directory"
+                exit 1
+            fi
+
             mkdir "$HOME/.vim"
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not create the .vim directory"
+                exit 1
+            fi
         else
             if [[ -e "$HOME/.vim/bundle/Vundle.vim" ]]
             then
                 rm -rf "$HOME/.vim/bundle/Vundle.vim"
+                if [[ $? -ne 0 ]]
+                then
+                    echo "Can not remove the bundle directory"
+                    exit 1
+                fi
             fi
         fi
     fi
@@ -69,6 +132,11 @@ function check_git() {
         then
             echo "Install git"
             sudo apt-get install --force-yes --yes git-core
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not install git-core"
+                exit 1
+            fi
         else
             echo "You must install git"
         fi
@@ -83,6 +151,11 @@ function check_vim() {
         then
             echo "Install vim"
             sudo apt-get install --force-yes --yes vim
+            if [[ $? -ne 0 ]]
+            then
+                echo "Can not install vim"
+                exit 1
+            fi
         else
             echo "You must install vim"
         fi
